a6fd599269e28fb6ce4621de848f5e4681ffb142
Mack Stanley <mlgprohacker@gmail.com>
2021-02-14 16:02:03 -0600

    personalize homepage

--- Controllers/ToDoItemsController.cs
+++ Controllers/ToDoItemsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -44,6 +45,7 @@ namespace ToDoList.Controllers
         }
 
         // GET: ToDoItems/Create
+        [Authorize]
         public IActionResult Create()
         {
             return View();
@@ -52,6 +54,7 @@ namespace ToDoList.Controllers
         // POST: ToDoItems/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Description,IsComplete")] ToDoItem toDoItem)
@@ -66,6 +69,7 @@ namespace ToDoList.Controllers
         }
 
         // GET: ToDoItems/Edit/5
+        [Authorize]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -84,6 +88,7 @@ namespace ToDoList.Controllers
         // POST: ToDoItems/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,IsComplete")] ToDoItem toDoItem)
@@ -117,6 +122,7 @@ namespace ToDoList.Controllers
         }
 
         // GET: ToDoItems/Delete/5
+        [Authorize]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -135,6 +141,7 @@ namespace ToDoList.Controllers
         }
 
         // POST: ToDoItems/Delete/5
+        [Authorize]
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
